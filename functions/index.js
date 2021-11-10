da3fb404cd85fda02497758d3e59ec24480a2522
Nuno Martins <nunoormaker21@gmail.com>
2021-11-10 01:06:30 +0000

    Created functions "generateRandomNonce", "createAccount" and "generateNonceForAccount" Fixed ESLint bugs

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,56 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// The Firebase Admin SDK to access Firestore.
+const admin = require('firebase-admin')
+admin.initializeApp()
+
+/** Utility functions **/
+const generateRandomNonce = () => parseInt(Math.random() * 1000000)
+
+exports.generateRandomNonce = functions.https.onRequest((req, res) => {
+    res.json({ nonce: generateRandomNonce() })
+})
+
+exports.createAccount = functions.https.onRequest(async (req, res) => {
+    try {
+        await admin.firestore().collection('users').doc(req.query.address).set({
+            address: req.query.address,
+            nonce: generateRandomNonce(),
+            username: '',
+        })
+
+        res.status(200).json({
+            msg: `New account created with ID ${req.query.address}`,
+            success: true,
+        })
+    } catch (err) {
+        res.status(400).json({
+            msg: err.message,
+            success: false,
+        })
+    }
+})
+
+exports.generateNonceForAccount = functions.https.onRequest(
+    async (req, res) => {
+        try {
+            await admin
+                .firestore()
+                .collection('users')
+                .doc(req.query.address)
+                .update({
+                    nonce: generateRandomNonce(),
+                })
+
+            res.status(200).json({
+                msg: `New nonce generated for account ${req.query.address}`,
+                success: true,
+            })
+        } catch (err) {
+            res.status(400).json({
+                msg: err.message,
+                success: false,
+            })
+        }
+    }
+)
