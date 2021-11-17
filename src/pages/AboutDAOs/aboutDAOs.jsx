b336722186530db5487e97d2888f587cba627414
R11manish <github.jmtss@gmail.com>
2021-11-17 08:28:28 +0530

    done with routing aboutDAOs page

---
+++ src/pages/AboutDAOs/aboutDAOs.jsx
@@ -0,0 +1,16 @@
+import Header from '../../components/Header'
+import Footer from '../../components/Footer'
+import Card from '../../components/Card'
+import { Route } from 'react-router-dom'
+
+const AboutDAOS = (props) => {
+    return (
+        <div>
+            <Header />
+            <h1>What are Daos?</h1>
+            <Footer />
+        </div>
+    )
+}
+
+export default AboutDAOS
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
 
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route
-  } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import Home from './Home'
 import DAO from './DAO'
 import Search from './Search'
+import AboutDAOS from './AboutDAOs/aboutDAOs'
 
-const App = props => {
+const App = (props) => {
     return (
         <Router>
             <Switch>
                 <Route exact path="/">
                     <Home />
+                </Route>
+                <Route exact path="/what-are-DAOs">
+                    <AboutDAOS />
                 </Route>
                 <Route path="/dao/:id">
                     <DAO />
