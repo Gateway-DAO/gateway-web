6414cb23c750eae56d0902d80caa1a0bfc969db0
arun <arunshekhar001@gmail.com>
2021-11-18 13:14:41 +0530

    what are Dao added, need refactAoring

---
+++ src/pages/AboutDAOs/Texts.jsx
@@ -0,0 +1,106 @@
+import * as Styled from './style'
+
+// const Text = {
+//     first: (
+//         <Styled.ContentContainer>
+//             <Styled.ParagraphContainer>
+//                 <Styled.BoldHeading>DAO Operating Systems</Styled.BoldHeading>
+               
+//             </Styled.ParagraphContainer>
+//         </Styled.ContentContainer>
+//     ),
+// }
+const Texts = [
+    {
+        heading: 'DAO Operating Systems',
+        subHeading1:
+            'The first chapter of DAOs are the operating systems used to create them.',
+        subHeading2:
+            'These projects offer different templates, frameworks and tools for communities to pool resources and start their first DAO.',
+        subHeading3:
+            'They commonly offer smart contracts and interfaces to facilitate on-chain actions for decentralized communities.',
+        subHeading4:
+            'DAO Operating Systems make it easy for anyone to start a DAO with limited technical skills.',
+    },
+    {
+        heading: 'Grants DAOs',
+        subHeading1: 'The first real use case for DAOs were grants.',
+        subHeading2:
+            'Communities donate funds and use a DAO to vote on how that capital is allocated to various contributors in the form of governance proposals.',
+        subHeading3:
+            'Governance of Grants DAOs was originally conducted through non-transferable shares, meaning participation was largely motivated by social capital over financial returns.',
+        subHeading4:
+            'Grants DAOs show that niche communities are more nimble in capital allocation than formal bodies.',
+    },
+    {
+        heading: 'Protocol DAOs',
+        subHeading1:
+            'Protocol DAOs transition power from a core team into the hands of the community, spawning a new way for projects to issue fungible tokens into the market.',
+        subHeading2:
+            'Whereas the first chapter of DAOs featured non-transferable shares, Protocol DAOs were the first to issue transferable ERC20 tokens with a secondary market value.',
+        subHeading3:
+            'These tokens are commonly used to govern protocols, meaning tokenholders have the sole authority to propose, vote on and implement changes to the underlying mechanics of the network.',
+        subHeading4:
+            'Projects commonly vote on how to distribute tokens, thus opening the doors to liquidity mining, yield farming, fair launches and everything in between.',
+        subHeading5:
+            'Protocol DAOs provided a framework for any network to issue a token that was (hopefully) owned and operated by its community.',
+    },
+    {
+        heading: 'Investment DAOs',
+        subHeading1:
+            'With Protocol DAOs bringing new tokens into the world, it only seemed logical that groups band together to invest in them.',
+        subHeading2:
+            'After a long period of non-profit DAOs, investment clubs flipped the switch for members to focus on generating a return.',
+        subHeading3:
+            'While these DAOs come with a lot more legal restrictions than a Grants DAO, they showed that any group of individuals could come together to invest larger amounts of capital with low barriers to entry.',
+        subHeading4:
+            'Investment DAOs allow members to pool capital and invest in projects at their earliest stages.',
+    },
+    {
+        heading: 'Service DAOs',
+        subHeading1:
+            'With so many tokens coming into the world, projects need talent. Service DAOs are talent allocators, using on-chain credentials to funnel and allocate resources from one DAO to another.',
+        subHeading2:
+            'Service DAOs create decentralized working groups for individuals to work for the open internet - essentially acting as crypto-native talent agencies.',
+        subHeading3:
+            'From legal to creative, governance to marketing, development to treasury management, Service DAOs create funnels to contract web3 mercenaries. Work is often rewarded with ERC20 tokens - providing ownership over the value created for a network.',
+        subHeading4:
+            'Service DAOs explore the future of work, and what employment looks like in crypto-native world.',
+    },
+    {
+        heading: 'Social DAOs',
+        subHeading1: 'What does it mean to be a member of a DAO?',
+        subHeading2:
+            'In an industry dominated by speculation, Social DAOs focus on social capital over financial capital. Social DAOs are the natural evolution of group chats, where friends become co-workers.',
+        subHeading3:
+            'Where social media turned everybody into a media company, Social DAOs turn every group chat into a digital business.',
+        subHeading4:
+            'They challenge what it means to be a part of a community and offer ways to lean into becoming a part of a digitally-native tribe.',
+        subHeading5:
+            'Social DAOs show that there is more to crypto than making a quick buck, and that the internet is the best place to meet people with similar interests.',
+    },
+    {
+        heading: 'Collector DAOs',
+        subHeading1:
+            'NFTs. You’ve heard of them - and for a while, you couldn’t ignore them.',
+        subHeading2:
+            'On the back of mainstream adoption, Collector DAOs popped up to collect them.',
+        subHeading3:
+            'Curator groups act as the underlying glue behind a specific artist, platform or series to help establish longevity.',
+        subHeading4:
+            'Collector DAOs seek to curate which NFTs have long-term value.',
+    },
+    {
+        heading: 'Media DAOs',
+        subHeading1:
+            'In an age when information is globally accessible, ownership of the narratives through which they are distributed should be, too.',
+        subHeading2:
+            'Media DAOs give that power back to those who consume the content.',
+        subHeading3:
+            'They break down the way in which writers, streamers, and readers engage with the content they release. Whether it be media mining programs to incentivize contributions or governance over which topics make the front page, Media DAOs turn consumption into a two-way street.',
+        subHeading4:
+            'Media DAOs share an outlet’s open agenda to spread awareness and news.',
+    }
+]
+    
+export default Texts;
--- src/pages/AboutDAOs/aboutDAOs.jsx
+++ src/pages/AboutDAOs/aboutDAOs.jsx
@@ -1,15 +1,142 @@
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
-import Card from '../../components/Card'
-import { Route } from 'react-router-dom'
+import Texts from './Texts';
+import DaoInfoCards from '../../components/DaoInfoCards';
+import * as Styled from './style'
 
 const AboutDAOS = (props) => {
     return (
-        <div>
+        <Styled.PageContainer>
             <Header />
-            <h1>What are Daos?</h1>
+            <Styled.CTABox>
+                <Styled.HeadingText>What are DAOs?</Styled.HeadingText>
+            </Styled.CTABox>
+            <Styled.BoxContainer>
+                <Styled.ContentContainer>
+                    <Styled.ParagraphContainer>
+                        DAOs or Decentralized Autonomous Organizations are a lot
+                        more than just a Discord channel with a native token.
+                        Rather, they are entities geared towards a shared
+                        purpose:
+                        <Styled.BoldText>
+                            the creation of value.
+                        </Styled.BoldText>
+                    </Styled.ParagraphContainer>
+                </Styled.ContentContainer>
+                <Styled.ContentContainer>
+                    <Styled.ParagraphContainer>
+                        <Styled.MediumText>
+                            What are their characteristics?
+                        </Styled.MediumText>
+                    </Styled.ParagraphContainer>
+                </Styled.ContentContainer>
+                <Styled.ContentContainer>
+                    <Styled.ParagraphContainer>
+                        <Styled.BoldHeading>Ownership</Styled.BoldHeading>
+                        DAOs distribute ownership to a variety of stakeholders
+                        in an ecosystem, including contributors, users,
+                        strategic partners, vendors, and so forth. They are
+                        owned by the people who create value in them.
+                    </Styled.ParagraphContainer>
+                </Styled.ContentContainer>
+
+                <Styled.ContentContainer>
+                    <Styled.ParagraphContainer>
+                        <Styled.BoldHeading>Organization</Styled.BoldHeading>
+                        Constituents can join a DAO and choose to contribute in
+                        the manner they find most compelling. There may be
+                        guidelines, but by and large, stakeholders choose their
+                        own labor and self-organize. DAO “workers” join in where
+                        and when they believe they canadd value and wish to do
+                        so.
+                    </Styled.ParagraphContainer>
+                </Styled.ContentContainer>
+
+                <Styled.ContentContainer>
+                    <Styled.ParagraphContainer>
+                        <Styled.BoldHeading>DAO Landscape</Styled.BoldHeading>
+                        Today, there are more than 100 DAOs managing over $10B
+                        in assets.
+                    </Styled.ParagraphContainer>
+                </Styled.ContentContainer>
+
+                <Styled.ContentContainer>
+                    <Styled.ParagraphContainer>
+                        <Styled.MediumText>Types of DAOs</Styled.MediumText>
+                    </Styled.ParagraphContainer>
+                </Styled.ContentContainer>
+                <Styled.ContentContainer>
+                    <Styled.ParagraphContainer>
+                        <Styled.BoldHeading>
+                            DAO Operating Systems
+                        </Styled.BoldHeading>
+                        The first chapter of DAOs are the operating systems used
+                        to create them.
+                        <br />
+                        <br />
+                        These projects offer different templates, frameworks and
+                        tools for communities to pool resources and start their
+                        first DAO.
+                        <br />
+                        <br />
+                        They commonly offer smart contracts and interfaces to
+                        facilitate on-chain actions for decentralized
+                        communities.
+                        <br />
+                        <br />
+                        DAO Operating Systems make it easy for anyone to start a
+                        DAO with limited technical skills.
+                    </Styled.ParagraphContainer>
+                </Styled.ContentContainer>
+
+                {Texts.map((text) => (
+                    <div>
+                        <Styled.ContentContainer>
+                            <Styled.ParagraphContainer>
+                                <Styled.BoldHeading>
+                                    {text.heading}
+                                </Styled.BoldHeading>
+                                {text.subHeading1}
+                                <br />
+                                <br />
+                                {text.subHeading2}
+                                <br />
+                                <br />
+                                {text.subHeading3}
+                                <br />
+                                <br />
+                                {text.subHeading4}
+                            </Styled.ParagraphContainer>
+                        </Styled.ContentContainer>
+                        <Styled.CardContainer>
+                            <DaoInfoCards />
+                        </Styled.CardContainer>
+                    </div>
+                ))}
+                <Styled.ContentContainer>
+                    <Styled.ParagraphContainer>
+                        <Styled.MediumText>Just DAO it</Styled.MediumText>
+                    </Styled.ParagraphContainer>
+                </Styled.ContentContainer>
+                <Styled.ContentContainer>
+                    <Styled.ParagraphContainer>
+                        To the DAO contributors of the world - keep up the great
+                        work.
+                        <br />
+                        <br />
+                        We’ve got a long way to go before DAOs have proven real
+                        traction at scale, but this snapshot should show that
+                        some of the world’s brightest minds are committed to
+                        making that happen.
+                        <br />
+                        <br />
+                        In the meantime, keep shipping and remember… when in
+                        doubt - Just DAO It.
+                    </Styled.ParagraphContainer>
+                </Styled.ContentContainer>
+            </Styled.BoxContainer>
             <Footer />
-        </div>
+        </Styled.PageContainer>
     )
 }
 
