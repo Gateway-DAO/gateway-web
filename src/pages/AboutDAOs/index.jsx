7e41c47c7a9af326bc9a36e32f8781abe22bd304
arun <arunshekhar001@gmail.com>
2021-11-22 04:13:52 +0530

    about-dao-page-complete-refractor

--- src/pages/AboutDAOs/index.jsx
+++ src/pages/AboutDAOs/index.jsx
@@ -1,142 +1,103 @@
+import React, { useEffect, useState } from 'react'
 import Header from '../../components/Header'
+import TitleSection from '../../components/AboutDaoComponents/TitleSection'
 import Footer from '../../components/Footer'
-import Texts from './Texts';
-import DaoInfoCards from '../../components/DaoInfoCards';
+import CardScrollWrapper from '../../components/AboutDaoComponents/CardScrollWrapper'
+import texts from '../../components/AboutDaoComponents/texts'
+import { allDocs } from '../../api/db'
+import whiteboard from '../../assets/about-dao-whiteboard.png'
 import * as Styled from './style'
 
 const AboutDAOS = (props) => {
+    const [cards, setCards] = useState([])
+
+    const fetchAllCards = async () => {
+        let allCards = await allDocs
+        let { docs } = allCards
+        let newCards = docs.map(async (doc) => {
+            const data = doc.data()
+            const id = doc.id
+            return { id, ...data }
+        })
+
+        const resolved = await Promise.all(newCards)
+        setCards(resolved)
+    }
+    useEffect(() => {
+        fetchAllCards()
+    }, [])
+
+    const typeOfDao = (type) => {
+        const socialDao = cards.filter((card) => card.categories.includes(type))
+        return socialDao
+    }
+
     return (
         <Styled.PageContainer>
             <Header />
-   
-                <Styled.BubbleImageContainer>
-                    <Styled.HeadingText>What are DAOs?</Styled.HeadingText>
-                </Styled.BubbleImageContainer>
-            <Styled.BoxContainer>
-            
-                <Styled.ContentContainer>
+            <TitleSection />
+            <Styled.MainWrapper>
+                <Styled.CenterWrapper>
                     <Styled.ParagraphContainer>
-                        DAOs or Decentralized Autonomous Organizations are a lot
-                        more than just a Discord channel with a native token.
-                        Rather, they are entities geared towards a shared
-                        purpose:
-                        <Styled.BoldText>
-                            the creation of value.
-                        </Styled.BoldText>
+                        {texts.definition[0]}
+                        <Styled.BoldText>{texts.definition[1]}</Styled.BoldText>
+                        <br/>
                     </Styled.ParagraphContainer>
-                </Styled.ContentContainer>
-                <Styled.ContentContainer>
-                    <Styled.ParagraphContainer>
-                        <Styled.MediumText>
-                            What are their characteristics?
-                        </Styled.MediumText>
-                    </Styled.ParagraphContainer>
-                </Styled.ContentContainer>
-                <Styled.ContentContainer>
-                    <Styled.ParagraphContainer>
-                        <Styled.BoldHeading>Ownership</Styled.BoldHeading>
-                        DAOs distribute ownership to a variety of stakeholders
-                        in an ecosystem, including contributors, users,
-                        strategic partners, vendors, and so forth. They are
-                        owned by the people who create value in them.
-                    </Styled.ParagraphContainer>
-                </Styled.ContentContainer>
-
-                <Styled.ContentContainer>
-                    <Styled.ParagraphContainer>
-                        <Styled.BoldHeading>Organization</Styled.BoldHeading>
-                        Constituents can join a DAO and choose to contribute in
-                        the manner they find most compelling. There may be
-                        guidelines, but by and large, stakeholders choose their
-                        own labor and self-organize. DAO “workers” join in where
-                        and when they believe they canadd value and wish to do
-                        so.
-                    </Styled.ParagraphContainer>
-                </Styled.ContentContainer>
-
-                <Styled.ContentContainer>
-                    <Styled.ParagraphContainer>
-                        <Styled.BoldHeading>DAO Landscape</Styled.BoldHeading>
-                        Today, there are more than 100 DAOs managing over $10B
-                        in assets.
-                    </Styled.ParagraphContainer>
-                </Styled.ContentContainer>
-
-                <Styled.ContentContainer>
-                    <Styled.ParagraphContainer>
-                        <Styled.MediumText>Types of DAOs</Styled.MediumText>
-                    </Styled.ParagraphContainer>
-                </Styled.ContentContainer>
-                <Styled.ContentContainer>
-                    <Styled.ParagraphContainer>
-                        <Styled.BoldHeading>
-                            DAO Operating Systems
-                        </Styled.BoldHeading>
-                        The first chapter of DAOs are the operating systems used
-                        to create them.
-                        <br />
-                        <br />
-                        These projects offer different templates, frameworks and
-                        tools for communities to pool resources and start their
-                        first DAO.
-                        <br />
-                        <br />
-                        They commonly offer smart contracts and interfaces to
-                        facilitate on-chain actions for decentralized
-                        communities.
-                        <br />
-                        <br />
-                        DAO Operating Systems make it easy for anyone to start a
-                        DAO with limited technical skills.
-                    </Styled.ParagraphContainer>
-                </Styled.ContentContainer>
-
-                {Texts.map((text) => (
-                    <div>
-                        <Styled.ContentContainer>
+                    <Styled.MediumText>
+                        What are their characteristics?
+                    </Styled.MediumText>
+                    {texts.characteristics.map((text) => (
+                        <Styled.ParagraphContainer>
+                            <Styled.BoldHeading>
+                                {text.title}
+                            </Styled.BoldHeading>
+                            {text.subtext}
+                        </Styled.ParagraphContainer>
+                    ))}
+                    <Styled.WhiteImage>
+                        <img style={{objectFit:"cover",width:"100%"}} src={whiteboard} alt="whiteboard.jpg" />
+                    </Styled.WhiteImage>
+                    <Styled.MediumText>Types of DAOs</Styled.MediumText>
+                </Styled.CenterWrapper>
+            </Styled.MainWrapper>
+            {texts.types.map((text) => (
+                <React.Fragment>
+                    <Styled.MainWrapper>
+                        <Styled.CenterWrapper>
                             <Styled.ParagraphContainer>
                                 <Styled.BoldHeading>
-                                    {text.heading}
+                                    {text.Name}
                                 </Styled.BoldHeading>
-                                {text.subHeading1}
-                                <br />
-                                <br />
-                                {text.subHeading2}
-                                <br />
-                                <br />
-                                {text.subHeading3}
-                                <br />
-                                <br />
-                                {text.subHeading4}
+                                {text.subtexts.map((subtext) => (
+                                    <React.Fragment>
+                                        {subtext} <br /> <br />
+                                    </React.Fragment>
+                                ))}
+                                <Styled.BoldText>
+                                    {text.boldText}
+                                </Styled.BoldText>
                             </Styled.ParagraphContainer>
-                        </Styled.ContentContainer>
-                        <Styled.CardContainer>
-                            <DaoInfoCards />
-                        </Styled.CardContainer>
-                    </div>
-                ))}
-                <Styled.ContentContainer>
+                        </Styled.CenterWrapper>
+                    </Styled.MainWrapper>
+                    <Styled.CardContainer>
+                        <CardScrollWrapper cards={typeOfDao(text.type)} />
+                    </Styled.CardContainer>
+                </React.Fragment>
+            ))}
+            <Styled.MainWrapper>
+                <Styled.CenterWrapper>
                     <Styled.ParagraphContainer>
                         <Styled.MediumText>Just DAO it</Styled.MediumText>
+                        <br/>
+                        {texts.footerText.map((text) => (
+                            <React.Fragment>
+                                {text} <br />
+                                <br />
+                            </React.Fragment>
+                        ))}
                     </Styled.ParagraphContainer>
-                </Styled.ContentContainer>
-                <Styled.ContentContainer>
-                    <Styled.ParagraphContainer>
-                        To the DAO contributors of the world - keep up the great
-                        work.
-                        <br />
-                        <br />
-                        We’ve got a long way to go before DAOs have proven real
-                        traction at scale, but this snapshot should show that
-                        some of the world’s brightest minds are committed to
-                        making that happen.
-                        <br />
-                        <br />
-                        In the meantime, keep shipping and remember… when in
-                        doubt - Just DAO It.
-                    </Styled.ParagraphContainer>
-                </Styled.ContentContainer>
-            </Styled.BoxContainer>
+                </Styled.CenterWrapper>
+            </Styled.MainWrapper>
             <Footer />
         </Styled.PageContainer>
     )
--- src/pages/AboutDAOs/style.jsx
+++ src/pages/AboutDAOs/style.jsx
@@ -1,11 +1,9 @@
 import styled from 'styled-components'
-import CTA_BG from '../../assets/bubbles.png'
 
 export const PageContainer = styled.main`
     background-color: transparent;
     height: 100%;
     position: relative;
-
     overflow-x: hidden;
 
     &:before {
@@ -17,58 +15,32 @@ export const PageContainer = styled.main`
         width: 100%;
     }
 `
-export const BubbleImageContainer = styled.div`
+export const MainWrapper = styled.div`
     display: flex;
-    width: 100vw;
-    height: 95vh;
-    background-image: url('${CTA_BG}');
-    background-position: center;
-    background-size: cover;
+    color: white;
+    width: 100%;
     justify-content: center;
 `
-
-export const HeadingText = styled.h2`
-    background: linear-gradient(
-        88.04deg,
-        #ee787b 22.54%,
-        #e153f2 41.08%,
-        #495be0 65.25%,
-        #6a39f3 86.1%
-    );
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
-    -moz-background-clip: text;
-    -moz-text-fill-color: transparent;
-    font-family: Poppins;
-    font-style: normal;
-    font-weight: bold;
-    font-size: 85px;
-    line-height: 64px;
-    /* or 100% */
-    letter-spacing: -0.015em;
-    display: flex;
-    align-self: center;
-    padding: 5px;
-
+export const CenterWrapper = styled.div`
+    color: white;
+    width: 55%;
+    justify-content: left;
     @media only screen and (max-width: 768px) {
-        font-size: 50px;
-        line-height: 54px;
-        margin: 0 30px;
+        width: 80%;
     }
 
     @media only screen and (max-width: 460px) {
-        font-size: 35px;
-        line-height: 44px;
-        margin: 0 25px;
+        width: 90%;
     }
 
     @media only screen and (max-width: 350px) {
-        font-size: 30px;
-        line-height: 38px;
-        margin: 0 20px;
+        width: 95%;
     }
 `
 
+export const WhiteImage = styled.div`
+    margin-bottom: 25px;
+`
 export const ParagraphContainer = styled.p`
     font-family: Be Vietnam;
     font-style: normal;
@@ -76,21 +48,8 @@ export const ParagraphContainer = styled.p`
     font-size: 21px;
     line-height: 31px;
     color: #e5e5e5;
-    width: 55%;
     margin-bottom: 25px;
     margin-top: 25px;
-`
-
-export const ContentContainer = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-`
-
-export const BoxContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
 `
 
 export const MediumTextContainer = styled.div`
@@ -123,39 +82,35 @@ export const MediumText = styled.h4`
     font-weight: bold;
     font-size: 36px;
     line-height: 64px;
-    text-align: left;
 
     /* or 100% */
     letter-spacing: -0.015em;
 
     @media only screen and (max-width: 768px) {
         font-size: 50px;
-        line-height: 54px;
-        margin: 0 30px;
     }
 
     @media only screen and (max-width: 460px) {
         font-size: 40px;
-        line-height: 44px;
-        margin: 0 25px;
     }
 
     @media only screen and (max-width: 350px) {
         font-size: 30px;
-        line-height: 38px;
-        margin: 0 20px;
     }
 `
 
 export const CardContainer = styled.div`
     margin-left: 20%;
-    margin-bottom: 5%;
+    margin-bottom: 25px;
     @media only screen and (max-width: 768px) {
+        width: 100%;
     }
 
     @media only screen and (max-width: 460px) {
+        width: 100%;
     }
 
     @media only screen and (max-width: 350px) {
+        width: 100%;
     }
 `
