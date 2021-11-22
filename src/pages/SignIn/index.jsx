2819bdf4b76f672f9d7d5b6a5e45b2e4171821d1
Nuno Martins <nunoormaker21@gmail.com>
2021-11-22 16:49:11 +0000

    Finished the SignIn page component

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,21 +1,35 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useContext } from 'react'
 
-import space from '../../utils/canvas';
+import space from '../../utils/canvas'
 
-import * as Styled from "./style"
-import Header from "../../components/Header"
-import Footer from "../../components/Footer"
+import * as Styled from './style'
+import Header from '../../components/Header'
+
+import { userContext } from '../../contexts/UserContext'
+import { Redirect } from 'react-router'
 
 const SignIn = () => {
-    useEffect(() => space(window.innerHeight, window.innerWidth), [window.innerHeight, window.innerWidth]);
+    const { signIn, loggedIn, userInfo } = useContext(userContext)
 
-    return (
+    useEffect(
+        () => space(window.innerHeight, window.innerWidth),
+        [window.innerHeight, window.innerWidth]
+    )
+
+    return loggedIn ? (
+        <Redirect to="/profile" />
+    ) : (
         <Styled.Container>
             <Header />
-                <Styled.MainBox>
-                    <Styled.SpaceBox id="space-canvas" />
-                    <p>Sign In</p>
-                </Styled.MainBox>
+            <Styled.MainBox>
+                <Styled.SpaceBox id="space-canvas" />
+                <Styled.MainText>Please, Sign In To Continue</Styled.MainText>
+                <Styled.Button>
+                    <Styled.ButtonText onClick={signIn}>
+                        Connect Wallet
+                    </Styled.ButtonText>
+                </Styled.Button>
+            </Styled.MainBox>
         </Styled.Container>
     )
 }
--- src/pages/SignIn/style.jsx
+++ src/pages/SignIn/style.jsx
@@ -20,12 +20,75 @@ export const Container = styled.main`
 `
 
 export const MainBox = styled.section`
-    margin: 100px 0;
+    display: flex;
     flex: 1;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 `
 
 export const SpaceBox = styled.canvas`
     position: absolute;
     top: 90px;
     z-index: -1;
+`
+
+export const MainText = styled.h1`
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 96px;
+    line-height: 150px;
+    /* or 94% */
+
+    text-align: center;
+    letter-spacing: -0.05em;
+
+    /* Background */
+    background: linear-gradient(88.04deg, #EE787B 22.54%, #E153F2 41.08%, #495BE0 65.25%, #6A39F3 86.1%);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent; 
+    -moz-background-clip: text;
+    -moz-text-fill-color: transparent;
+`
+
+export const Button = styled.a`
+    box-shadow: 0px 6px 15px rgba(255, 0, 184, 0.3);
+    border-radius: 20px;
+
+    border: double 1px transparent;
+    background-image:   linear-gradient(#170627, #170627), 
+                        linear-gradient(90deg, #FF00B8 0%, #7E3BDC 50.52%, #0075FF 100%);
+    background-origin: border-box;
+    background-clip: content-box, border-box;
+
+    &:hover {
+        cursor: pointer;
+    }
+`
+
+export const ButtonText = styled.p`
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 13px;
+    line-height: 19px;
+    /* identical to box height */
+
+    letter-spacing: 0.05em;
+    text-transform: uppercase;
+
+    color: #E5E5E5;
+
+    margin: 10px 20px;
+
+    @media only screen and (max-width: 1170px) {
+        margin: 8px 16px;
+        font-size: 11px;
+    }
+
+    @media only screen and (max-width: 768px) {
+        margin: 6px 12px;
+        font-size: 9px;
+    }
 `
