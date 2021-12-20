43ce12308f7234170286f85e4cd0b82b35443910
R11manish <github.jmtss@gmail.com>
2021-12-20 18:00:55 +0530

    improve user experience while signing out

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,20 +1,21 @@
 import React, { createContext, useContext, useEffect, useState } from 'react'
 import { useHistory } from 'react-router-dom'
+import  { Redirect } from 'react-router-dom'
 
 // Web3
 import { CONNECTORS } from '../utils/web3'
 import { useWeb3React } from '@web3-react/core'
 
 // AWS/GraphQL
-import Amplify, { Auth } from "aws-amplify";
-import awsconfig from "../aws-exports";
-import { getNonce } from "../api/database/getNonce";
+import Amplify, { Auth } from 'aws-amplify'
+import awsconfig from '../aws-exports'
+import { getNonce } from '../api/database/getNonce'
 import { getUser as getUserQuery } from '../graphql/queries'
 import { useLazyQuery, gql } from '@apollo/client'
-import { Hub } from '@aws-amplify/core';
-import useUpdateUser from '../api/database/useUpdateUser';
+import { Hub } from '@aws-amplify/core'
+import useUpdateUser from '../api/database/useUpdateUser'
 
-Amplify.configure(awsconfig);
+Amplify.configure(awsconfig)
 
 export const userContext = createContext({})
 const { Provider } = userContext
@@ -30,8 +31,14 @@ export const UserProvider = ({ children }) => {
     const [loading, setLoading] = useState(false)
     const [userInfo, setUserInfo] = useState(null)
 
-    const [getUser, { data: userData, loading: userLoading, called }] = useLazyQuery(gql(getUserQuery));
-    const { updateUser, data: userUpdateData, error: userUpdateError, loading: userUpdateLoading } = useUpdateUser();
+    const [getUser, { data: userData, loading: userLoading, called }] =
+        useLazyQuery(gql(getUserQuery))
+    const {
+        updateUser,
+        data: userUpdateData,
+        error: userUpdateError,
+        loading: userUpdateLoading,
+    } = useUpdateUser()
 
     const web3 = useWeb3React()
     const history = useHistory()
@@ -43,21 +50,27 @@ export const UserProvider = ({ children }) => {
     // AWS
     const signIn = async () => {
         try {
-            !web3.active && await activateWeb3()
+            !web3.active && (await activateWeb3())
             setLoggingIn(true)
 
-            const { data } = await getNonce(web3.account);
+            const { data } = await getNonce(web3.account)
             console.log(data)
-            
+
             const signer = web3.library.getSigner()
 
-            const signature = await signer.signMessage(data.getAuthenticationNonce.nonce)
+            const signature = await signer.signMessage(
+                data.getAuthenticationNonce.nonce
+            )
 
-            const user = await Auth.signIn(data.getAuthenticationNonce.userId);
+            const user = await Auth.signIn(data.getAuthenticationNonce.userId)
             const res = await Auth.sendCustomChallengeAnswer(
                 user,
-                JSON.stringify({ signature, publicAddress: web3.account, nonce: data.getAuthenticationNonce.nonce })
-            );
+                JSON.stringify({
+                    signature,
+                    publicAddress: web3.account,
+                    nonce: data.getAuthenticationNonce.nonce,
+                })
+            )
 
             console.log(res)
 
@@ -66,11 +79,11 @@ export const UserProvider = ({ children }) => {
                     variables: {
                         id: res.username,
                     },
-                });
-                setUserInfo(userDB.data.getUser);
-                setLoggedIn(true);
+                })
+                setUserInfo(userDB.data.getUser)
+                setLoggedIn(true)
             }
-            
+
             setLoggingIn(false)
         } catch (err) {
             console.error(err)
@@ -80,20 +93,21 @@ export const UserProvider = ({ children }) => {
 
     // AWS
     const userSignOut = async () => {
-        await Auth.signOut();
-        setLoggedIn(false);
-        setLoggingIn(false);
-        setUserInfo(null);
-    };
+        await Auth.signOut()
+        setLoggedIn(false)
+        setLoggingIn(false)
+        setUserInfo(null)
+        
+    }
 
     const updateUserInfo = async (id, info) => {
         const user = await updateUser({
             variables: {
                 input: info,
                 condition: {
-                    id: { eq: id }
-                }
-            }
+                    id: { eq: id },
+                },
+            },
         })
 
         setUserInfo(user.data.updateUser)
@@ -106,59 +120,60 @@ export const UserProvider = ({ children }) => {
                 setLoggedIn(true)
             } else {
                 Auth.signOut().then(() => {
-                    setLoggedIn(false);
-                    setUserInfo(null);
-                    setLoggingIn(false);
+                    setLoggedIn(false)
+                    setUserInfo(null)
+                    setLoggingIn(false)
                 })
             }
         }
     }, [web3.account])
 
     const listener = async ({ payload: { event, data } }) => {
-        console.log('event', event);
+        console.log('event', event)
         switch (event) {
             case 'signIn':
                 const userDB = await getUser({
                     variables: {
                         id: data.username,
                     },
-                });
-                setUserInfo(userDB.data.getUser);
-                setLoggedIn(true);
-                break;
+                })
+                setUserInfo(userDB.data.getUser)
+                setLoggedIn(true)
+                break
             case 'signOut':
-                setLoggedIn(false);
-                setLoggingIn(false);
-                setUserInfo(null);
-                break;
+                setLoggedIn(false)
+                setLoggingIn(false)
+                setUserInfo(null)
+                break
             default:
         }
-    };
+    }
 
     useEffect(() => {
-        Hub.listen('auth', listener);
-        return () => Hub.remove('auth', listener);
-    });
+        Hub.listen('auth', listener)
+        return () => Hub.remove('auth', listener)
+    })
 
     useEffect(() => {
         const callback = async () => {
             try {
-                const { username, signInUserSession } = await Auth.currentAuthenticatedUser();
+                const { username, signInUserSession } =
+                    await Auth.currentAuthenticatedUser()
                 if (username) {
                     const userDB = await getUser({
                         variables: {
                             id: username,
                         },
-                    });
+                    })
                     setUserInfo(userDB.data.getUser)
-                    setLoggedIn(true);
+                    setLoggedIn(true)
                 }
             } catch (e) {
-                console.log(e);
+                console.log(e)
             }
-        };
-        callback();
-    }, []);
+        }
+        callback()
+    }, [])
 
     return (
         <Provider
