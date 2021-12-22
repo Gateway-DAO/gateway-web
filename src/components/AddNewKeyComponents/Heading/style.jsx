b5d3b3e1f895dcfdba6b9593f9dc92f448813324
Arun Shekhar <arunshekhar001@gmail.com>
2021-12-22 20:03:40 +0530

    gate pages added

---
+++ src/components/AddNewKeyComponents/Heading/style.jsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+    margin: 5px auto 50px auto;
+    
+`
+export const Text = styled.h1`
+    text-align: center;
+    padding: 0;
+    font-family: Poppins;
+    letter-spacing: 1px;
+    background: linear-gradient(
+        88.04deg,
+        #ee787b 22.54%,
+        #e153f2 41.08%,
+        #495be0 65.25%,
+        #6a39f3 86.1%
+    );
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    -moz-background-clip: text;
+    -moz-text-fill-color: transparent;
+    font-weight: bold;
+    font-size: 86px;
+    /* line-height: 64px; */
+    /* or 100% */
+    letter-spacing: -0.015em;
+`
+
---
+++ src/pages/AddNewKey/AddGovernanceSnapshot/style.js
@@ -0,0 +1,48 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    background: #170627;
+`
+export const MarginWrapper = styled.div`
+    margin: auto 20rem;
+`
+export const MarginWrapperSecondary = styled.div`
+    padding: 0 50px;
+`
+export const TaskBox = styled.div`
+    display: flex;
+    height: 80px;
+    width: 250px;
+    color: #fff;
+    border-radius: 5px;
+    font-size: 150%;
+    justify-content: center;
+    border: ${(props) =>
+        props.active ? '1px solid #7E3BDC' : '1px solid rgba(255, 255, 255, 0.1)'};
+    border-radius: 5px;
+    cursor: pointer;
+    text-decoration: none;
+    background: ${(props) => (props.active ? '#220A38' : 'none')};
+`
+
+export const TasksBox = styled.div`
+    display: flex;
+    color: #444;
+    align-self: center;
+    justify-content:space-between;
+    margin-bottom:40px;
+`
+export const TaskBoxText = styled.div`
+    display: flex;
+    align-items: center;
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 13px;
+    line-height: 19px;
+    text-align: center;
+    letter-spacing: 0.05em;
+    text-transform: uppercase;
+    color: rgba(165, 165, 165, 1);
+    padding: 0px 20px;
+`
