572dce00f339f405b99237ba11d38fdee75e514e
R11manish <github.jmtss@gmail.com>
2022-01-20 21:07:08 +0530

    fixed categories remove section

--- src/pages/AddGateForm/Components/SearhedItem/index.jsx
+++ src/pages/AddGateForm/Components/SearhedItem/index.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import * as Styled from "./styles"
 
-const SearchedItem =({val})=>{
+const SearchedItem =({val , id , setCategoryList , categoryList})=>{
+
+    const removeCategories = (index) => {
+        if (categoryList.length === 1) {
+            alert('You have to put at least one Category')
+            return false
+        }
+        setCategoryList(
+            categoryList.filter((value, i) => i !== id)
+        )
+    }
 
     return(
         <Styled.Container>
             <Styled.Text>
                 {val}
             </Styled.Text>
-            <Styled.Cross>+</Styled.Cross>
+            <Styled.Cross onClick={removeCategories}>+</Styled.Cross>
         </Styled.Container>
     )
 }
