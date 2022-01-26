a8a5c08fd78444ea79ebb05d82185f40a15a9e22
R11manish <github.jmtss@gmail.com>
2022-01-26 17:42:16 +0530

    styling pagination

--- src/pages/Search/component/Pagination/index.jsx
+++ src/pages/Search/component/Pagination/index.jsx
@@ -1,13 +1,32 @@
 import * as Styled from './style'
+import { useState } from 'react'
 import { CircularButton, PaginationBox } from './style'
 
 const Pagination = () => {
+
+    // const {pageDetails , setPageDetails} = useState({
+    //     pageNumbers : []
+    // })
+    let totalPage = 61;
+    let postPerPage = 8;
+    const pageNumbers = []
+
+    for(let i =1 ; i <= Math.ceil(totalPage / postPerPage) && i <=5; i++){
+        pageNumbers.push(i)
+    }
     return (
         <Styled.PaginationContainer>
             <PaginationBox>
                 <Styled.CircularButton>{'<'}</Styled.CircularButton>
-                <Styled.CircularButton>{'1'}</Styled.CircularButton>
-                <Styled.CircularButton value={true}>{'>'}</Styled.CircularButton>
+                {
+                    pageNumbers.map((element) =>
+                        <Styled.CircularButton select={element==3}>{element}</Styled.CircularButton>
+                    )
+                }
+
+                <Styled.CircularButton value={true}>
+                    {'>'}
+                </Styled.CircularButton>
             </PaginationBox>
         </Styled.PaginationContainer>
     )
