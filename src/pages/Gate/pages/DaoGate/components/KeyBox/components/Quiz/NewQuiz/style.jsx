20a5e303b5d417c6cc94a267e75e38b8553d8098
R11manish <github.jmtss@gmail.com>
2022-02-07 14:25:48 +0530

    added add links page

--- src/pages/Gate/pages/DaoGate/components/KeyBox/components/Quiz/NewQuiz/style.jsx
+++ src/pages/Gate/pages/DaoGate/components/KeyBox/components/Quiz/NewQuiz/style.jsx
@@ -9,9 +9,6 @@ export const Container = styled.div`
 export const Box = styled.div`
     display: flex;
     flex-direction: column;
-    /* align-items: center;
-    justify-content: center; */
-    //height: 90vh;
     margin: 20px 25vw;
 `
 
