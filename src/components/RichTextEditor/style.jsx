8a05a39df514ee4f9b0790cfaf3d47bb4e877d49
Jatin Arora <arorajatin003@gmail.com>
2022-03-02 19:51:57 +0530

    Adding Tooltip

--- src/components/RichTextEditor/style.jsx
+++ src/components/RichTextEditor/style.jsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const RichEditor = styled.div`
     font-family: Be Vietnam;
@@ -15,11 +15,10 @@ export const RichEditor = styled.div`
     padding: 15px 0;
     color: #e5e5e5;
     & .ql-editor {
-        min-height: 200px;
+      min-height:200px;
     }
-    & .ql-toolbar,
-    .ql-container,
-    .ql-snow {
-        border: 1px solid rgba(255, 255, 255, 0.2);
+
+    & .ql-toolbar, .ql-container, .ql-snow {
+      border: 1px solid rgba(255, 255, 255, 0.2);
     }
-`;
+`
