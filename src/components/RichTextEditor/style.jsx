cdbf890f99d221622b4e01e23f041d0ecb03b6ca
alchemist0404 <maletiger000@gmail.com>
2022-03-13 01:48:46 +0100

    update

--- src/components/RichTextEditor/style.jsx
+++ src/components/RichTextEditor/style.jsx
@@ -1,16 +1,10 @@
-import styled from 'styled-components';
-
-const FilledEditor = `
-    background: #220A38;
-    border: 1px solid #7E3BDC;
-    box-sizing: border-box;
-`;
+import styled from "styled-components";
 
 export const RichEditor = styled.div`
     font-family: Be Vietnam;
     font-style: normal;
     font-weight: normal;
-    font-size: 16px;
+    font-size: 12px;
     line-height: 20px;
     position: relative;
     display: -ms-flexbox;
@@ -20,42 +14,11 @@ export const RichEditor = styled.div`
     border-radius: 10px;
     padding: 15px 0;
     color: #e5e5e5;
-
     & .ql-editor {
-        min-height: 200px;
-        font-size: 16px;
+      min-height:200px;
     }
 
-    & .ql-container {
-        border-bottom-left-radius: 5px;
-        border-bottom-right-radius: 5px;
+    & .ql-toolbar, .ql-container, .ql-snow {
+      border: 1px solid rgba(255, 255, 255, 0.2);
     }
-
-    & .ql-toolbar {
-        border-top-left-radius: 5px;
-        border-top-right-radius: 5px;
-    }
-
-    & .ql-toolbar,
-    .ql-container,
-    .ql-snow {
-        ${(props) =>
-            props.filled
-                ? FilledEditor
-                : 'border: 1px solid rgba(255, 255, 255, 0.2);'}
-    }
-
-    & .ql-toolbar .ql-stroke {
-        fill: none;
-        stroke: #a5a5a5;
-    }
-
-    & .ql-toolbar .ql-fill {
-        fill: #a5a5a5;
-        stroke: none;
-    }
-
-    & .ql-toolbar .ql-picker {
-        color: #a5a5a5;
-    }
-`;
+`
