c983d18a6149bfba7775e08d187733434c10fda4
alchemist0404 <maletiger000@gmail.com>
2022-04-02 02:25:34 +0200

    fixed responsiveness of gates page

--- src/components/GateCard/index.jsx
+++ src/components/GateCard/index.jsx
@@ -129,129 +129,131 @@ const GateCard = ({ gate, viewAsMember }) => {
 
     return (
         <Styled.GateCardBox>
-            <Styled.GateBanner
-                src={`https://ipfs.io/ipfs/${gate.badge.ipfsURL}`}
-                onClick={goToGate}
-            >
-                {false && (
-                    <Styled.EditContainer>
-                        <EditIcon />
-                    </Styled.EditContainer>
-                )}
+            <Styled.GateContainer>
+                <Styled.GateBanner
+                    src={`https://ipfs.io/ipfs/${gate.badge.ipfsURL}`}
+                    onClick={goToGate}
+                >
+                    {false && (
+                        <Styled.EditContainer>
+                            <EditIcon />
+                        </Styled.EditContainer>
+                    )}
 
-                {/* <Styled.NFTBadgeContainer>
-                    <Styled.SimpleText>Badge</Styled.SimpleText>
-                    <Styled.GuildName>{gate.badge.name}</Styled.GuildName>
-                </Styled.NFTBadgeContainer> */}
-                {/*
-                <Styled.PeopleInvolved>
-                    <PfpBox text="4 people have earned it." />
-                </Styled.PeopleInvolved>
-                */}
-            </Styled.GateBanner>
-            <Styled.CategoryList>
-                {gate.categories.map((category, idx) => (
-                    <Styled.Category key={idx}>
-                        <Styled.CategoryLink to='/'>
-                            {category}
-                        </Styled.CategoryLink>
-                    </Styled.Category>
-                ))}
-            </Styled.CategoryList>
-            <Styled.CardBody onClick={goToGate}>
-                <Styled.CardTitle>{gate.name}</Styled.CardTitle>
-                <Styled.CardDesc>
-                    {gate.description.length > numberOfWords
-                        ? gate.description
-                              .slice(0, numberOfWords - 3)
-                              .concat('...')
-                        : gate.description}
-                </Styled.CardDesc>
-            </Styled.CardBody>
-            <Styled.InfoContainer onClick={goToGate}>
-                <Styled.InfoBox>
-                    <Styled.MediumHeading>Badge</Styled.MediumHeading>
-                    <Styled.GuildName>
-                        {gate.badge.name.slice(0, 16) +
-                            (gate.badge.name.length > 16 ? '...' : '')}
-                    </Styled.GuildName>
-                </Styled.InfoBox>
-                {/* gate.preRequisites && (
+                    {/* <Styled.NFTBadgeContainer>
+                        <Styled.SimpleText>Badge</Styled.SimpleText>
+                        <Styled.GuildName>{gate.badge.name}</Styled.GuildName>
+                    </Styled.NFTBadgeContainer> */}
+                    {/*
+                    <Styled.PeopleInvolved>
+                        <PfpBox text="4 people have earned it." />
+                    </Styled.PeopleInvolved>
+                    */}
+                </Styled.GateBanner>
+                <Styled.CategoryList>
+                    {gate.categories.map((category, idx) => (
+                        <Styled.Category key={idx}>
+                            <Styled.CategoryLink to='/'>
+                                {category}
+                            </Styled.CategoryLink>
+                        </Styled.Category>
+                    ))}
+                </Styled.CategoryList>
+                <Styled.CardBody onClick={goToGate}>
+                    <Styled.CardTitle>{gate.name}</Styled.CardTitle>
+                    <Styled.CardDesc>
+                        {gate.description.length > numberOfWords
+                            ? gate.description
+                                .slice(0, numberOfWords - 3)
+                                .concat('...')
+                            : gate.description}
+                    </Styled.CardDesc>
+                </Styled.CardBody>
+                <Styled.InfoContainer onClick={goToGate}>
                     <Styled.InfoBox>
-                        <Styled.MediumHeading>
-                            PRE REQUISITE
-                        </Styled.MediumHeading>
+                        <Styled.MediumHeading>Badge</Styled.MediumHeading>
+                        <Styled.GuildName>
+                            {gate.badge.name.slice(0, 16) +
+                                (gate.badge.name.length > 16 ? '...' : '')}
+                        </Styled.GuildName>
+                    </Styled.InfoBox>
+                    {/* gate.preRequisites && (
+                        <Styled.InfoBox>
+                            <Styled.MediumHeading>
+                                PRE REQUISITE
+                            </Styled.MediumHeading>
+                            <Styled.InfoText>
+                                Gateway.DAO.Verfication
+                            </Styled.InfoText>
+                        </Styled.InfoBox>
+                    ) */}
+                    <Styled.InfoBox>
+                        {gate.keysNumber && (
+                            <>
+                                <Styled.MediumHeading>
+                                    KEYS REQUIRED
+                                </Styled.MediumHeading>
+                                <Styled.KeyBox>
+                                    <Styled.Circle>
+                                        <CircularProgressbar
+                                            value={
+                                                data?.getGateStatusByUserID
+                                                    ?.items[0]?.keysDone || 0
+                                            }
+                                            minValue={0}
+                                            maxValue={gate.keysNumber}
+                                            strokeWidth={20}
+                                        />
+                                    </Styled.Circle>
+                                    <Styled.SmallText>
+                                        {data?.getGateStatusByUserID?.items[0]
+                                            ?.keysDone || 0}{' '}
+                                        of {gate.keysNumber}
+                                    </Styled.SmallText>
+                                </Styled.KeyBox>
+                            </>
+                        )}
+                    </Styled.InfoBox>
+                    {/* </Styled.Column>
+                    <Styled.Column> */}
+                    <Styled.InfoBox>
+                        <Styled.MediumHeading>EARNERS</Styled.MediumHeading>
                         <Styled.InfoText>
-                            Gateway.DAO.Verfication
+                            {gate.holders}{' '}
+                            {gate.holders !== 1 ? 'people have' : 'person has'}{' '}
+                            earned it.
                         </Styled.InfoText>
                     </Styled.InfoBox>
-                ) */}
-                <Styled.InfoBox>
-                    {gate.keysNumber && (
-                        <>
-                            <Styled.MediumHeading>
-                                KEYS REQUIRED
-                            </Styled.MediumHeading>
-                            <Styled.KeyBox>
-                                <Styled.Circle>
-                                    <CircularProgressbar
-                                        value={
-                                            data?.getGateStatusByUserID
-                                                ?.items[0]?.keysDone || 0
-                                        }
-                                        minValue={0}
-                                        maxValue={gate.keysNumber}
-                                        strokeWidth={20}
-                                    />
-                                </Styled.Circle>
-                                <Styled.SmallText>
-                                    {data?.getGateStatusByUserID?.items[0]
-                                        ?.keysDone || 0}{' '}
-                                    of {gate.keysNumber}
-                                </Styled.SmallText>
-                            </Styled.KeyBox>
-                        </>
-                    )}
-                </Styled.InfoBox>
-                {/* </Styled.Column>
-                <Styled.Column> */}
-                <Styled.InfoBox>
-                    <Styled.MediumHeading>EARNERS</Styled.MediumHeading>
-                    <Styled.InfoText>
-                        {gate.holders}{' '}
-                        {gate.holders !== 1 ? 'people have' : 'person has'}{' '}
-                        earned it.
-                    </Styled.InfoText>
-                </Styled.InfoBox>
-                {/* </Styled.Column> */}
-                {/* </Styled.InfoBox> */}
-            </Styled.InfoContainer>
-            <Styled.ActivityBox>
-                <Styled.ActionButton onClick={goToGate}>
-                    <Styled.ButtonText>{getButtonText()}</Styled.ButtonText>
-                </Styled.ActionButton>
-                {/*isAdmin && !viewAsMember && (
-                    <Styled.PublishContainer>
-                        <Styled.PublishText>PUBLISH</Styled.PublishText>
-                        <Switch
-                            checked={checked}
-                            onChange={toggleGatePublished}
-                            offColor='#FF003D'
-                            onColor='#27D5A2'
-                            onHandleColor='#FFFFFF'
-                            handleDiameter={20}
-                            uncheckedIcon={false}
-                            checkedIcon={false}
-                            boxShadow='0px 1px 5px rgba(0, 0, 0, 0.6)'
-                            activeBoxShadow='0px 0px 1px 10px rgba(0, 0, 0, 0.2)'
-                            height={18}
-                            width={44}
-                            className='react-switch'
-                            id='material-switch'
-                        />
-                    </Styled.PublishContainer>
-                )*/}
-            </Styled.ActivityBox>
+                    {/* </Styled.Column> */}
+                    {/* </Styled.InfoBox> */}
+                </Styled.InfoContainer>
+                <Styled.ActivityBox>
+                    <Styled.ActionButton onClick={goToGate}>
+                        <Styled.ButtonText>{getButtonText()}</Styled.ButtonText>
+                    </Styled.ActionButton>
+                    {/*isAdmin && !viewAsMember && (
+                        <Styled.PublishContainer>
+                            <Styled.PublishText>PUBLISH</Styled.PublishText>
+                            <Switch
+                                checked={checked}
+                                onChange={toggleGatePublished}
+                                offColor='#FF003D'
+                                onColor='#27D5A2'
+                                onHandleColor='#FFFFFF'
+                                handleDiameter={20}
+                                uncheckedIcon={false}
+                                checkedIcon={false}
+                                boxShadow='0px 1px 5px rgba(0, 0, 0, 0.6)'
+                                activeBoxShadow='0px 0px 1px 10px rgba(0, 0, 0, 0.2)'
+                                height={18}
+                                width={44}
+                                className='react-switch'
+                                id='material-switch'
+                            />
+                        </Styled.PublishContainer>
+                    )*/}
+                </Styled.ActivityBox>
+            </Styled.GateContainer>
         </Styled.GateCardBox>
     );
 };
