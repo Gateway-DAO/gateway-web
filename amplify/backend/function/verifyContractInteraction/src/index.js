d0d58c358ba623726a31c59f08d9cbe6d0d25665
Nuno Carvalho <nunoormaker21@gmail.com>
2022-04-11 18:16:01 +0100

    Initialized Nuno's environment on Amplify

--- amplify/backend/function/verifyContractInteraction/src/index.js
+++ amplify/backend/function/verifyContractInteraction/src/index.js
@@ -1,3 +1,17 @@
+/*
+Use the following code to retrieve configured secrets from SSM:
+
+const aws = require('aws-sdk');
+
+const { Parameters } = await (new aws.SSM())
+  .getParameters({
+    Names: ["API_KEY"].map(secretName => process.env[secretName]),
+    WithDecryption: true,
+  })
+  .promise();
+
+Parameters will be of the form { Name: 'secretName', Value: 'secretValue', ... }[]
+*/
 /* Amplify Params - DO NOT EDIT
 	API_GATEWAY_DAOTABLE_ARN
 	API_GATEWAY_DAOTABLE_NAME
